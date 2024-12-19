ebd2343aabf7688dfa122fad1e62c81aba278de7
Thunder3355 <putriamaksym@gmail.com>
2024-12-20 00:48:40 +0100

    Update Program.cs nt

--- API/Program.cs
+++ API/Program.cs
@@ -1,53 +1,38 @@
-using System;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
 using API.Data;
-using API.Middleware;
 using API.Extensions;
+using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+
+var app = builder.Build();
+
+// Middleware
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapFallbackToController("Index", "Fallback");
+app.MapControllers();
+
+// Выполнение миграции и добавление данных
+using (var scope = app.Services.CreateScope())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddApplicationServices(builder.Configuration);
-        builder.Services.AddIdentityServices(builder.Configuration);
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        app.UseMiddleware<ExceptionMiddleware>();
-
-        app.UseCors(x => x.AllowAnyHeader()
-            .AllowAnyMethod()
-            .WithOrigins("http://localhost:4200", "https://localhost:4200"));
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.UseDefaultFiles();
-        app.UseStaticFiles();
-
-        app.MapFallbackToController("Index", "Fallback");
-        app.MapControllers();
-
-        using var scope = app.Services.CreateScope();
-        var services = scope.ServiceProvider;
-        try
-        {
-            var context = services.GetRequiredService<DataContext>();
-            context.Database.Migrate();
-            Seed.SeedUsers(context).Wait();
-        }
-        catch (Exception ex)
-        {
-            var logger = services.GetRequiredService<Microsoft.Extensions.Logging.ILogger<Program>>();
-            logger.LogError(ex, "An error occurred during migration");
-        }
-
-        app.Run();
-    }
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
 }
+
+app.Run();
